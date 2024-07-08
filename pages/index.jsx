1021c6562dc582cde046f0dd74df1de66c581126
Victor Alves <victor.souza@gmail.com>
2024-07-08 15:56:33 -0300

    adicionando pagina inicial

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <h1
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+     Teste Live
+    </h1>
+  );
+}
+
+export default Home;
